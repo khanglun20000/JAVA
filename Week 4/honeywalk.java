af832489e4cadba753f808201ff02c2d2d056612
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2022-11-15 00:25:35 +0100

    Add solution

---
+++ Week 4/honeywalk.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class honeywalk {
+    public static void main(String[] args) {
+    
+        int array[][][] = new int[29][29][15];
+
+        int originX = 14;
+        int originY = 14;
+
+        int maxWidth = 29;
+        int maxHeight = 29;
+        int maxStep = 15;
+
+        for(int i = 0; i < maxHeight; i++) {
+            for (int j = 0; j < maxWidth; j++) {
+                if (i == originX & j == originY) {
+                    array[i][j][0] = 1;
+                }
+                else array[i][j][0] = 0;
+            }
+        }
+        
+        for (int k = 1; k < maxStep; k++) {
+            for(int i = 0; i < maxHeight; i++) {
+                for (int j = 0; j < maxWidth; j++) {
+                    int sum = 0;
+                    if (i != 0) {
+                        sum = sum + array[i-1][j][k-1];
+                    }
+                    if (i != 0 & j != 28 ) {
+                        sum = sum + array[i-1][j+1][k-1];
+                    }
+                    if (i != 28) {
+                        sum = sum + array[i+1][j][k-1];
+                    }
+                    if (j != 28) {
+                        sum = sum + array[i][j+1][k-1];
+                    }
+                    if (j != 0) {
+                        sum = sum + array[i][j-1][k-1];
+                    }
+                    if (i != 28 & j != 0) {
+                        sum = sum + array[i+1][j-1][k-1];
+                    }
+                    array[i][j][k] = sum;
+                }
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int input = sc.nextInt();
+            System.out.println(array[originX][originY][input]);
+        }
+    }
+}
