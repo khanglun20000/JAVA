b270eb8af392f92dc84405ad17a1f1f1e4b6675c
hoangviet10101 <117586045+hoangviet10101@users.noreply.github.com>
2022-11-20 16:39:48 +0100

    Add files via upload

---
+++ W1/T6/eligibility.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class eligibility{
+    public static void main(String[] args){
+        String[] arr_in = new String[50];
+        String[] name = new String[20];
+        String[] begin_day = new String[20];
+        String[] birth_day = new String[20];
+        String[] courses = new String[20];
+        int num=0;
+        
+        Scanner myObj = new Scanner(System.in); 
+        num = myObj.nextInt();
+        
+        for (int i=0; i<num; i++){
+            arr_in[i] = myObj.nextLine();
+        }
+
+        int size = arr_in.length;
+
+        for (int i=0; i<=size*3; i+=3){
+            name[i] = arr_in[i];
+            begin_day[i] = arr_in[i+1];
+            birth_day[i] = arr_in[i+2];
+            courses[i] = arr_in[i+3]; 
+        }
+
+        for (int i=0; i<num; i++){
+            System.out.println(name[i]);
+            if(Integer.parseInt(begin_day[i].substring(0, 4))>= 2010){
+                System.out.print(" eligible");
+                continue;
+            } else if(Integer.parseInt(birth_day[i].substring(0, 4))>= 1991){
+                System.out.print(" eligible");
+                continue;
+            } else if(Integer.parseInt(courses[i]) >40){
+                System.out.print(" ineligible");
+                continue;
+            } else {
+                System.out.print(" coach petitions");
+            }
+            
+        }
+
+        myObj.close();
+    }
+
+}
