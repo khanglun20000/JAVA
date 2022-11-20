c95419d3a5df3da6d55c21e3ab62def126527e7c
hoangviet10101 <117586045+hoangviet10101@users.noreply.github.com>
2022-11-20 16:47:49 +0100

    Add files via upload

---
+++ W2/T4/bus.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ProblemN {
+    public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in);
+        int num = myObj.nextInt();
+        int [] num_arr = new int[num];
+
+
+        for (int i=0; i<num; i++){
+            num_arr[i] = myObj.nextInt();
+        }
+
+        for (int i=0; i<num; i++){
+            double n  = 0;
+            for (int j=0; j<num_arr[i]; j++){
+                n = (n+0.5)*2;
+            }
+            System.out.printf("%1.0f\n", n);
+        }
+        
+        myObj.close();
+    }
+}
