0e8e321a246db8d09552b30475e25f9b90057141
Ha Quach Phu Thanh <thanhbo025@gmail.com>
2022-11-20 21:20:55 +0100

    Adding solutions to Exercise 4 of Week 3

--- W3/T4/Animal.java
+++ W3/T4/Animal.java
@@ -1,8 +1,48 @@
-public class Animal {
+public class Animal implements Cloneable {
+    String name;
+    int age;
+
     public static void testClassMethod() {
         System.out.println("The static method in Animal");
     }
     public void testInstanceMethod() {
         System.out.println("The instance method in Animal");
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "This animal's name is " + this.name + " and is " + this.age +" years old";
+    }
+
+    @Override
+    public Object clone() {
+        try{  
+            return super.clone();  
+        }catch(Exception e){ 
+            return null; 
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        Animal otherAnimal = (Animal) otherObject;
+        if (this.name == otherAnimal.name & this.age == otherAnimal.age) {
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int sum = 0;
+        for (int i = 0; i < this.name.length(); i++) {
+            sum = sum + (int) this.name.charAt(i);
+        }
+        return sum * age;
     }
 }
--- W3/T4/Cat.java
+++ W3/T4/Cat.java
@@ -1,4 +1,6 @@
 public class Cat extends Animal {
+    String breed;
+
     public static void testClassMethod() {
         System.out.println("The static method in Cat");
     }
@@ -6,10 +8,74 @@ public class Cat extends Animal {
         System.out.println("The instance method in Cat");
     }
 
+    public Cat(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "This cat's name is " + this.name + ", is " + this.age +" years old and is a " + this.breed + " cat";
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        Cat otherCat = (Cat) otherObject;
+        if (super.equals(otherObject) & this.breed == otherCat.breed) {
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int sum = 0;
+        for (int i = 0; i < this.name.length(); i++) {
+            sum = sum + (int) this.name.charAt(i);
+        }
+        return sum * age;
+    }
+
     public static void main(String[] args) {
-        Cat myCat = new Cat();
+        Cat myCat = new Cat("Alex", 17, "Stray");
         Animal myAnimal = myCat;
         Animal.testClassMethod();
         myAnimal.testInstanceMethod();
+        myCat.testInstanceMethod();
+        Cat.testClassMethod();
+
+
+
+        Animal firstAnimal = new Animal("Bucky", 12);
+        Cat firsCat = new Cat("Joe", 9, "Stray");
+        
+        System.out.println(firstAnimal.toString());
+        System.out.println(firstAnimal.hashCode());
+
+        System.out.println(firsCat.toString());
+        System.out.println(firsCat.hashCode());
+
+        System.out.println("After cloning 2 new Animal and Cat objects:");
+        
+        Animal secondAnimal = (Animal) firstAnimal.clone();
+        Cat secondCat = (Cat) firsCat.clone();
+
+        System.out.println(secondAnimal.toString());
+        System.out.println(secondAnimal.hashCode());
+
+        System.out.println(secondCat.toString());
+        System.out.println(secondCat.hashCode());
+
+        System.out.println("After creating 2 new Animal and Cat objects:");
+
+        Animal thirdAnimal = new Animal("John", 15);
+        Cat thirdCat = new Cat("Trang", 1, "Bengal");
+
+        System.out.println(thirdAnimal.toString());
+        System.out.println(thirdAnimal.hashCode());
+
+        System.out.println(thirdCat.toString());
+        System.out.println(thirdCat.hashCode());
+
     }
 }
